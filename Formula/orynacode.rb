21f0c5c36c299f6ff8d74863b9d78dc94e8f574f
Ory <oryna.app@gmail.com>
2026-06-19 14:12:04 +0800

    Update to v1.16.19

--- Formula/orynacode.rb
+++ Formula/orynacode.rb
@@ -1,14 +1,14 @@
 class Orynacode < Formula
   desc "Terminal AI coding agent"
   homepage "https://oryna.ai"
-  version "1.16.18"
+  version "1.16.19"
 
   if Hardware::CPU.arm?
     url "https://github.com/oryna-ai/orynacode/releases/download/v#{version}/orynacode-darwin-arm64.zip"
-    sha256 "646824123f128dcaf8415a4716863e489f9ce718d0c5fc2b405d7208a34a728a"
+    sha256 "e6e4543733e1a6dff4c2d2bd488ae6b079ed7e5dbfab3003a76433642801762d"
   else
     url "https://github.com/oryna-ai/orynacode/releases/download/v#{version}/orynacode-darwin-x64.zip"
-    sha256 "49f7f68a4ecace94dcbafcdcac645307c808f78f9a29171bd2f8ceb826d708d9"
+    sha256 "e31519e03a5a816e271fae5b6fa8da5cbd87fe06b39d5068e425f8b100eddae0"
   end
 
   license "MIT"
